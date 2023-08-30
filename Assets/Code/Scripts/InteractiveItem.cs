00ccfe520039209bacf2588ec5d0136fd340e312
Todd Spainhour <59629794+ToddSpainhour@users.noreply.github.com>
2023-08-29 21:37:33 -0500

    onclick saves interactive item name to inventory list
    - clicking on an interactive item (in this case the key), push its name to a list that logs its contents

--- Assets/Code/Scripts/InteractiveItem.cs
+++ Assets/Code/Scripts/InteractiveItem.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI; // use this with legacy text
 
@@ -9,6 +10,7 @@ public class InteractiveItem : MonoBehaviour
     public string itemName;
     public string itemDescription;
     public bool isInventoryItem;
+    public static List<string> playerInventory = new List<string>();
 
     // creating these GameObject variables here also creates a slot back in Unity
     // with the purpose of connecting another GameObject to this variable
@@ -23,16 +25,45 @@ public class InteractiveItem : MonoBehaviour
     void Start()
     {
         // Start() is useful to set upvariables, read preferences, and make connections with other GameObjects
-
-        dynamicTitleName.GetComponent<UnityEngine.UI.Text>().text = itemName;
-        dynamicTitleDescription.GetComponent<UnityEngine.UI.Text>().text = itemDescription;
-        dynamicTitleIsInventoryItem.GetComponent<UnityEngine.UI.Text>().text = isInventoryItem.ToString();
-
     }
 
 
     public void DisplayInteractiveItemDetails()
     {
-     // Update() is the place to handle the frame update for the GameObject  
+        // Update() is the place to handle the frame update for the GameObject  
+
+        // this is the long way to do this but it works
+        dynamicTitleName.GetComponent<UnityEngine.UI.Text>().text = itemName;
+        dynamicTitleDescription.GetComponent<UnityEngine.UI.Text>().text = itemDescription;
+        dynamicTitleIsInventoryItem.GetComponent<UnityEngine.UI.Text>().text = isInventoryItem.ToString();
+
+        // you can also save these as variables to make things a little shorter like this...
+
+        // Text tempTitle = dynamicTitleName.GetComponent<UnityEngine.UI.Text>();
+        // tempTitle.text = itemName;
+
+        // Text tempDescription = dynamicTitleName.GetComponent<UnityEngine.UI.Text>();
+        // tempDescription.text = itemDescription;
+
+        // Text tempIsInventoryItem = dynamicTitleName.GetComponent<UnityEngine.UI.Text>();
+        // tempIsInventoryItem.text = isInventoryItem.ToString();
+    }
+
+    public void RemoveInteractiveItemDetails()
+    {
+        dynamicTitleName.GetComponent<UnityEngine.UI.Text>().text = " ";
+        dynamicTitleDescription.GetComponent<UnityEngine.UI.Text>().text = " ";
+        dynamicTitleIsInventoryItem.GetComponent<UnityEngine.UI.Text>().text = " ";
+    }
+
+    public void AddItemToInventory()
+    {
+        string itemToPush = dynamicTitleName.GetComponent<UnityEngine.UI.Text>().text;
+        playerInventory.Add(itemToPush);
+
+        for (int i = 0; i < playerInventory.Count; i++)
+        {
+            Debug.Log("playerInventory[i]: " + playerInventory[i]);
+        }
     }
 }
